0956e3fb80eae67d3ca7476072a42779109d40b2
Ahmed-Abdelaziz-Dev <aabdelaziz@next.so>
2024-04-05 17:14:58 +0200

    Add: axios package + axios instance

---
+++ src/api/axios-instance.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const abortController = new AbortController();
+
+const axiosInstance = axios.create({
+  baseURL: "",
+  withCredentials: true,
+  signal: abortController.signal,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    config.headers["Content-Type"] = "application/json";
+    config.headers["Access-Control-Allow-Origin"] = "true";
+    config.headers["Access-Control-Allow-Credentials"] = "true";
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.log(error)
+  }
+);
+
+export default axiosInstance;
